028e06e68a1776e1b8be3fa5af8becdd02122946
jespin457 <jorge.e.espinoza.57@gmail.com>
2021-07-22 02:04:44 -0500

    MVP completed; must work on extensions and making UX nicer

--- popup.js
+++ popup.js
@@ -36,29 +36,51 @@ const database = {
   },
 
   'forEach' : {
-    'mdn' : 'blah',
-    'mdnCode':'blah',
-    'w3':'blah',
-    'stack':'blah'
+    'mdn' : 'The forEach() method executes a provided function once for each array element.',
+    'mdnCode':`const array1 = ['a', 'b', 'c'];\n
+
+    array1.forEach(element => console.log(element));\n
+    
+    // expected output: "a"\n
+    // expected output: "b"\n
+    // expected output: "c"`,
+    'w3':'The forEach() method calls a function once for each element in an array, in order. forEach() is not executed for array elements without values.',
+    'stack':'https://stackoverflow.com/questions/9329446/for-each-over-an-array-in-javascript'
   },
 
   'addEventListener' : {
-    'mdn' : 'blah',
-    'mdnCode':'blah',
-    'w3':'blah',
-    'stack':'blah'
+    'mdn' : 'The EventTarget method addEventListener() sets up a function that will be called whenever the specified event is delivered to the target. Common targets are Element, Document, and Window, but the target may be any object that supports events (such as XMLHttpRequest).',
+    'mdnCode':`function eventHandler(event) {
+      if (event.type == 'fullscreenchange') {
+        /* handle a full screen toggle */
+      } else /* fullscreenerror */ {
+        /* handle a full screen toggle error */
+      }
+    }`,
+    'w3':'The addEventListener() method attaches an event handler to the specified element.',
+    'stack': 'https://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick'
   },
 
   'fetch' : {
-    'mdn' : 'blah',
-    'mdnCode':'blah',
-    'w3':'blah',
-    'stack':'blah'
+    'mdn' : 'The Fetch API provides an interface for fetching resources (including across the network). It will seem familiar to anyone who has used XMLHttpRequest, but the new API provides a more powerful and flexible feature set.',
+    'mdnCode':"fetch(url)\n.then(resp => resp.json())",
+    'w3':'The Fetch API interface allows web browser to make HTTP requests to web servers. No need for XMLHttpRequest anymore.',
+    'stack':'https://stackoverflow.com/questions/29775797/fetch-post-json-data'
   },
+
+  'CQtest' : {
+    'mdn' : 'A summary from Mozilla MDN documentation',
+    'mdnCode': 'An example of the function being used',
+    'w3': 'A snippet from a W3Schools article on the function',
+    'stack': 'Link to a relevant post from Stack Overflow',
+  }
 }
 //must wait for document to load!
 document.addEventListener('DOMContentLoaded', () => {
 
+  document.getElementById("languages").addEventListener("change", function() {
+    console.log();
+  })
   //event listener for when the button is clicked!
   document.getElementById("querySearch").addEventListener("click", function() {
     console.log("entered");
@@ -77,8 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
           currentEle.innerHTML = "Relevant StackOverflow Post";
           currentEle.href = database[query][key];
           currentEle.target = "_blank";
-          currentEle.rel = "noopener noreferrer";
-          console.log("goes in here!");
+          currentEle.rel = "noopener noreferrer"; 
         } else {
           currentEle.innerHTML = database[query][key]
         };
@@ -95,11 +116,15 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("exited");
   });
 
-});
 
-//fetch
-//bind
-//object?
-//array?
-//forEach
-//addEventListener
+  //attempt to change font size with buttons
+  // document.getElementById("decFont").addEventListener("click", function() {
+  //   let keys = ['mdn', 'mdnCode', 'w3', 'stack'];
+
+  //   keys.forEach(key => {
+  //     let currentEle = document.getElementById(key);
+
+  //     currentEle.style.fontSize--;
+  //   });
+  // });
+});
